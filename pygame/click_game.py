0ac5a8ce6e9924a1e877fceceea7c04558ab1a34
Breanna83 <113620339+Breanna83@users.noreply.github.com>
2023-02-09 12:17:44 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,4 +1,4 @@
-mago = Actor('character')
+mago = Actor('mago')
 mago.pos = 100,50
 
 WIDTH = 500
@@ -6,10 +6,33 @@ HEIGHT = mago.height + 20
 
 def draw():
     screen.clear
-    screen.fill((255 , 255 , 255))
+    screen.fill((225, 225 , 225))
     mago.draw()
 
 def update():
     mago.left = mago.left + 2
     if mago.left > WIDTH:
         mago.right = 0
+
+def on_mouse_down(pos):
+    if  mago.collidepoint(pos):
+        print ("swag")
+    else:
+        print("no swag :/")
+    if mago.collidepoint(pos):
+        sounds.splat.play()
+        mago.image='mago_click'
+    if mago.collidepoint(pos):
+        set_mago_click()
+
+def set_mago_click():
+    mago.image = 'mago_click'
+    sounds.splat.play()
+
+def set_mago_normal():
+    mago.image = 'mago'
+
+def set_mago_click():
+    mago.image = 'mago_click'
+    sounds.splat.play()
+    clock.schedule_unique(set_mago_normal, 3.0)
